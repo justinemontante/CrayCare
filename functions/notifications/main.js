e20a775e6089ae777b6eb8269cf8673d45c03c6f
Justine Montante <justinemontante04@gmail.com>
2026-08-22 20:26:39 +0800

    Export hardware assignment guards

--- functions/notifications/main.js
+++ functions/notifications/main.js
@@ -1,4 +1,10 @@
 const existingFunctions = require("./index");
 const dailySummaryFunctions = require("./daily_summary");
+const assignmentGuardFunctions = require("./assignment_guard");
 
-Object.assign(exports, existingFunctions, dailySummaryFunctions);
+Object.assign(
+  exports,
+  existingFunctions,
+  dailySummaryFunctions,
+  assignmentGuardFunctions,
+);
